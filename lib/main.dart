bcf616260317ee2fea339786c8aad1b1801bbcf3
danieljoinedgithub <145357879+danieljoinedgithub@users.noreply.github.com>
2025-01-15 11:01:28 +0000

    Bundle release update
    build.gradle updated
    signed app
    included multiple app icons, removed standard icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bola 8 Mágica',
       theme: ThemeData(
         colorScheme: const ColorScheme.dark(),
         useMaterial3: true,
